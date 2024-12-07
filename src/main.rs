3763d1fd4643e08e36d710072652b283cada566a
yazoink <yazoink@firemail.cc>
2024-12-07 14:13:41 +1100

    a

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use std::{
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
+    path::PathBuf,
     env,
 };
 use rand::seq::SliceRandom;
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
-    let file_name = "data/words.txt";
+    let file_paths = get_file_path_array();
     let mut len = 3;
     if argv.len() > 1 {
         if argv.len() == 3 {
@@ -37,7 +38,14 @@ fn main() {
             }
         }
     }
-    let words = array_from_file(file_name);
+    let file_path: String = match get_file_path(file_paths) {
+        Some(path) => path,
+        None => {
+            eprintln!("words.txt not found.");
+            std::process::exit(1);
+        },
+    };
+    let words = array_from_file(file_path);
     let password = generate_password(&words, len);
     println!("{}", password);
 }
@@ -48,6 +56,41 @@ fn array_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     buf.lines()
         .map(|l| l.expect("Could not parse line."))
         .collect()
+}
+
+fn file_exists(file_name: &str) -> bool {
+    let path = Path::new(file_name);
+    match path.try_exists() {
+        Ok(true) => true,
+        Ok(false) => false,
+        Err(e) => {
+            eprintln!("Error checking file existence: {}", e);
+            std::process::exit(1);
+        },
+    }
+}
+
+fn get_file_path_array() -> Vec<String> {
+    let mut file_paths: Vec<String> = Vec::new();
+    if let Ok(home) = env::var("HOME") {
+        let config_path = PathBuf::from(home)
+            .join(".config")
+            .join("pswdrs")
+            .join("words.txt");
+        file_paths.push(config_path.to_string_lossy().into_owned())
+    }
+    file_paths.push("/usr/share/pswdrs/words.txt".to_string());
+    file_paths.push("data/words.txt".to_string());
+    file_paths
+}
+
+fn get_file_path(file_paths: Vec<String>) -> Option<String> {
+    for file_path in file_paths {
+        if file_exists(&file_path) {
+            return Some(file_path);
+        }
+    }
+    None
 }
 
 fn help() {
